ef36914c54ae9fa9a5f22dc45815e29f6a24fc4a
shaancse08 <shaancse08@gmail.com>
2023-09-18 11:54:45 +0530

    Deployed Version of Express App

--- index.js
+++ index.js
@@ -1,23 +1,18 @@
 const express = require("express");
 const app = express();
-
 const mongoose = require("mongoose");
 const usersRouter = require("./routes/users");
+const port = process.env.PORT || 3000;
 
 app.use(express.json())
 
-mongoose.connect(
-  "mongodb+srv://shaancse08:CBx6oiboM6fR8bOT@expressbtp.x4r8yav.mongodb.net/"
-);
-const db = mongoose.connection;
-db.on("error", (error) => console.error(error));
-db.once("open", () => console.log("Db Connected Successfully"));
+
 
 app.use("/users", usersRouter)
 
 
 
 
-app.listen(4000, () => {
+app.listen(port, () => {
   console.log(`Server has started at http://localhost:3000`);
 });
